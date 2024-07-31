1a26895184f971fb996a065c5324bc4102232ee2
Matic Vrenko <matic.vrenko@razum.si>
2024-07-31 10:55:43 +0200

    Initial Commit

---
+++ NavigationPOC/NavigationPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPOCApp.swift
+//  NavigationPOC
+//
+//  Created by Vrenko on 31. 7. 24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
